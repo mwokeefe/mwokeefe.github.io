3f51ca2575790b057882bf42859c64da5e24fe85
Michael O'Keefe <michaelokeefe2711@hotmail.co.uk>
2023-08-28 20:06:21 +0100

    feat: add notes page

--- compile.sh
+++ compile.sh
@@ -2,6 +2,10 @@
 
 commit_message=$1
 
+git add content
+git commit -m "$1"
+git push -u origin dev
+
 pelican content -o output -s publishconf.py
 ghp-import -m "$1" --no-jekyll -b main output
 git push origin main
