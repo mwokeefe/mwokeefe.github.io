1781522a0d80c0d1f412171dffb9fbcdc0991b29
Michael O'Keefe <michael.o'keefe@raytheon.com>
2024-06-26 15:37:55 +0100

    try nested folder again

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,7 +28,8 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-# Heirarchical pages
-PAGE_URL = "{slug}/"
-PAGE_SAVE_AS = "{slug}/index.html"
-SLUGIFY_SOURCE = "basename"
+ARTICLE_PATHS = [
+    "articles",
+]
+ARTICLE_URL = "articles/{slug}.html"
+ARTICLE_SAVE_AS = "articles/{slug}.html"
